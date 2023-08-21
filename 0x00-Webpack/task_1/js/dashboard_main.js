1d210604ad2530d5a45cf801f9ad8fddb5eeb3bf
ZwingliCaleb <calebzwingli@gmail.com>
2023-08-21 20:34:24 +0300

    update dashboard main

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import _ from 'lodash';
+import _, { update } from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -7,11 +7,13 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
-
-function updateCounter() {
-  count++;
-  $("#count").html(`${count} clicks on the button`);
+const updateCounter = () => {
+    let count = $('#count').html() || 0;
+    $('button').on('click', () => {
+        count++;
+        $('#count').html(`${count} clicks on the button`);
+    });
 };
 
-$('button').on('click', _.debounce(updateCounter, 500));
+_.debounce(updateCounter, 500);
+updateCounter();
