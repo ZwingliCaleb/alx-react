a5401cfd99f6beae6a0681e820613edd287633e3
ZwingliCaleb <calebzwingli@gmail.com>
2023-08-31 20:38:51 +0300

    Modify notification test file

--- 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from "./Notifications";
-import '../../config/setupTests'
+import NotificationItem from './NotificationItem';
+import '../../config/setupTests';
 
 describe('<Notifications />', () => {
     it('renders without crashing', () => {
         shallow(<Notifications />);
     });
 
-    it('renders three list items', () => {
+    it('renders NotificationItem elements', () => {
         const wrapper = shallow(<Notifications />);
-        const listItems = wrapper.find('li');
-        expect(listItems).toHaveLength(3);
+        const listItems = wrapper.find('ul');
+        expect(listItems.children()).toHaveLength(3);
+        listItems.forEach((node) => {
+            expect(node.equals(<NotificationItem />));
+        });
     });
     it('renders the text Here is the list of notifications', () => {
         const wrapper = shallow(<Notifications />);
