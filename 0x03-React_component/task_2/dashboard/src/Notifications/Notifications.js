124581e042ee9c369f9df01ca0f0f867b5d63f6c
ZwingliCaleb <calebzwingli@gmail.com>
2023-09-11 14:58:12 +0300

    make changes to notifications.js and test file

--- 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.js
@@ -4,55 +4,68 @@ import closeIcon from "../assets/close-icon.png";
 // import { getLatestNotification } from "../utils/utils";
 import NotificationItem from "./NotificationItem";
 import PropTypes from 'prop-types';
-//import NotificationItemShape from './NotificationItemShape'
+import NotificationItemShape from './NotificationItemShape'
 
-function Notifications({displayDrawer, listNotifications}) {
-  return (
-      <>
-          { displayDrawer ? (
-          <div className='flexitem'>
-          <div className='menuItem'>
-                  <p>Your notifications</p>
-          </div>
-          <div className='Notifications'>
-              <button style={{float: 'right'}} aria-label='Close' 
-              onClick={ () => {
-                  console.log('Close button has been clicked')}
-                  }>
-                      <img src={closeIcon} alt='close button' />
-              </button>
+class Notifications extends Component {
+  constructor(props) {
+      super(props);
+      this.markAsRead = this.markAsRead.bind(this);
+  }
+  
+  markAsRead(id) {
+      console.log(`Notification ${id} has been marked as read`)
+  }
 
-              {listNotifications.length === 0 ? (
-                  <p>No notifications for now</p>
-              ) : (
-                <>
-                  <p>Here is the list of notifications</p>
-
-                  <ul>
-                      {listNotifications.map((notification) => (
-                          <NotificationItem key={notification.id} type={notification.type} value={notification.value} html={notification.html}/>
-                      ))}
-                  </ul>
-                </>
-              )}
-          </div>
-          </div>
-          ) : (
+  render () {
+      return (
+          <>
+              { this.props.displayDrawer ? (
+              <div className='flexitem'>
               <div className='menuItem'>
-                  <p>Your notifications</p>
-          </div>
-          )}
-          
-      </>
-  );
-};
+                      <p>Your notifications</p>
+              </div>
+              <div className='Notifications'>
+                  <button style={{float: 'right'}} aria-label='Close' 
+                  onClick={ () => {
+                      console.log('Close button has been clicked')}
+                      }>
+                          <img src={closeIcon} alt='close button' />
+                  </button>
+                  
+                  {this.props.listNotifications.length === 0 ? (
+                      <p>No new notification for now</p>
+                  ) : (
+                      <>
+                          <p>Here is the list of notifications</p>
+                          <ul>
+                              {this.props.listNotifications.map(notification => (
+                              <NotificationItem key={notification.id} type={notification.type} value={notification.value} html={notification.html} 
+                              markAsRead={this.markAsRead} id={notification.id} />
+                              ))}
+                          </ul>
+                          </>
+                  )}
+              </div>
+              </div>
+              ) : (
+                  <div className='menuItem'>
+                      <p>Your notifications</p>
+              </div>
+              )}
+              
+          </>
+      );
+  }
+}
 
 Notifications.propTypes = {
-  displayDrawer: PropTypes.bool
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape)
 };
 
 Notifications.defaultProps = {
   displayDrawer: false,
+  listNotifications: []
 };
 
 export default Notifications;
--- 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,47 +1,51 @@
 /* eslint-disable jest/valid-expect */
 import React from "react";
 import { shallow } from "enzyme";
-import { getLatestNotification } from "../utils/utils";
+//import { getLatestNotification } from "../utils/utils";
 import Notifications from "./Notifications";
 import NotificationItem from "./NotificationItem";
 import '../../config/setupTests'
 
 describe('<Notifications />', () => {
-  it('renders without crashing', () => {
-      shallow(<Notifications />);
-  });
+    it('renders without crashing', () => {
+        shallow(<Notifications />);
+    });
 
-  it('renders NotificationItem elements', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true}/>);
-      const listItems = wrapper.find('ul');
-      expect(listItems.children()).toHaveLength(3);
-      listItems.forEach((node) => {
-          expect(node.equals(<NotificationItem />));
+    it('renders menuItem class when displayDrawer is false', () => {
+        const component = shallow(<Notifications displayDrawer={false}/>);
+        const menu = component.find('div.menuItem');
+        const mainNoti = component.find('div.Notifications');
+
+        expect(menu.exists()).toBe(true);
+    });
+
+    it('renders menuItem and notifications class when displayDrawer is true', () => {
+        const component = shallow(<Notifications displayDrawer={true}/>);
+        const menu = component.find('div.menuItem');
+        const mainNoti = component.find('div.Notifications');
+
+        expect(menu.exists()).toBe(true);
+        expect(mainNoti.exists()).toBe(true);
+    });
+
+    it('renders correctly if you pass an empty array or if you don’t pass the listNotifications property', () => {
+        const wrapper = shallow(<Notifications displayDrawer={true}/>)
+        expect(wrapper.find(NotificationItem)).toHaveLength(0);
+        expect(wrapper.text()).toContain('No new notification for now');
+        expect(wrapper.text()).not.toContain('Here is the list of notifications');
+    });
+
+    it('renders notification list correctly with notifications', () => {
+        const listNotifications = [
+          { id: 1, type: 'default', value: 'New course available', html: null },
+          { id: 2, type: 'urgent', value: 'New resume available', html: null },
+        ];
+    
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+        const notificationItems = wrapper.find(NotificationItem);
+    
+        expect(notificationItems).toHaveLength(listNotifications.length);
+        expect(wrapper.text()).toContain('Here is the list of notifications');
+        expect(wrapper.text()).not.toContain('No new notification for now');
       });
-      expect(listItems.childAt(0).html()).toEqual('<li data-notification-type="default">New course available</li>');
-      expect(listItems.childAt(1).html()).toEqual('<li data-notification-type="urgent">New resume available</li>');
-      expect(listItems.childAt(2).html()).toEqual(`<li data-urgent=\"true\">${getLatestNotification()}</li>`);
-  });
-  it('renders the text Here is the list of notifications', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true} />);
-      expect(wrapper.contains(<p>Here is the list of notifications</p>)).toBe(true);
-  });
-
-  it('renders menuItem class when displayDrawer is false', () => {
-      const component = shallow(<Notifications displayDrawer={false}/>);
-      const menu = component.find('div.menuItem');
-      const mainNoti = component.find('div.Notifications');
-
-      expect(menu.exists()).toBe(true);
-  });
-
-  it('renders menuItem and notifications class when displayDrawer is true', () => {
-      const component = shallow(<Notifications displayDrawer={true}/>);
-      const menu = component.find('div.menuItem');
-      const mainNoti = component.find('div.Notifications');
-
-      expect(menu.exists()).toBe(true);
-      expect(mainNoti.exists()).toBe(true);
-  });
-
 })
