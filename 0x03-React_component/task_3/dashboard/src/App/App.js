fb622f51bebb121402392ecaeaf27b3c2798248f
ZwingliCaleb <calebzwingli@gmail.com>
2023-09-11 15:32:36 +0300

    Use the new components in App.js

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Notifications from '../Notifications/Notifications';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
 import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
+import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
+import BodySection from './BodySection';
 
 class App extends Component {
   constructor(props) {
@@ -15,15 +17,20 @@ class App extends Component {
   }
 
   listCourses = [
-    {id: 1, name: 'ES6', credit: 60},
-    {id: 2, name: "Webpack", credit: 20},
-    {id: 3, name: "React", credit: 40},
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
   ];
 
   listNotifications = [
-    {id: 1, type: 'default', value: 'New course available', html: null},
-    {id: 2, type: 'urgent', value: 'New resume available', html: null},
-    {id: 3, type: 'urgent', value: null, html: { __html : getLatestNotification()}, },
+    { id: 1, type: 'default', value: 'New course available', html: null },
+    { id: 2, type: 'urgent', value: 'New resume available', html: null },
+    {
+      id: 3,
+      type: 'urgent',
+      value: null,
+      html: { __html: getLatestNotification() },
+    },
   ];
 
   handleKeyDown(e) {
@@ -40,21 +47,31 @@ class App extends Component {
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyDown);
   }
- 
+
   render() {
     return (
       <div>
-        <Notifications displayDrawer={true} listNotifications={this.listNotifications}/>
+        <Notifications displayDrawer={true} listNotifications={this.listNotifications} />
         <div className="App">
           <Header />
-          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login />}
+          {this.props.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={this.listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
           <Footer />
+          <BodySection title="News from School">
+            <p>Some random text about school news.</p>
+          </BodySection>
         </div>
       </div>
     );
   }
-  
-};
+}
 
 App.defaultProps = {
   isLoggedIn: false,
