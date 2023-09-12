2cd180f1e7b4e32a1afecafda55d5c2342eaeb19
ZwingliCaleb <calebzwingli@gmail.com>
2023-09-12 13:09:43 +0300

    add new test cases

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,7 +1,6 @@
 /* eslint-disable jest/valid-expect */
 import React from "react";
 import { shallow } from "enzyme";
-//import { getLatestNotification } from "../utils/utils";
 import Notifications from "./Notifications";
 import NotificationItem from "./NotificationItem";
 import '../../config/setupTests'
@@ -14,7 +13,6 @@ describe('<Notifications />', () => {
     it('renders menuItem class when displayDrawer is false', () => {
         const component = shallow(<Notifications displayDrawer={false}/>);
         const menu = component.find('div.menuItem');
-        const mainNoti = component.find('div.Notifications');
 
         expect(menu.exists()).toBe(true);
     });
@@ -47,5 +45,42 @@ describe('<Notifications />', () => {
         expect(notificationItems).toHaveLength(listNotifications.length);
         expect(wrapper.text()).toContain('Here is the list of notifications');
         expect(wrapper.text()).not.toContain('No new notification for now');
-      });
-})
+    });
+
+    it('does not rerender when updating with the same list', () => {
+        const listNotifications = [
+          { id: 1, type: 'default', value: 'New course available', html: null },
+          { id: 2, type: 'urgent', value: 'New resume available', html: null },
+        ];
+
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+        const instance = wrapper.instance(); // Get the component instance
+
+        const shouldUpdateSpy = jest.spyOn(instance, 'shouldComponentUpdate');
+
+        wrapper.setProps({ displayDrawer: true, listNotifications: listNotifications });
+
+        expect(shouldUpdateSpy).toHaveBeenCalledTimes(1); // It should not rerender
+    });
+
+    it('rerenders when updating with a longer list', () => {
+        const listNotifications1 = [
+          { id: 1, type: 'default', value: 'New course available', html: null },
+        ];
+
+        const listNotifications2 = [
+          { id: 1, type: 'default', value: 'New course available', html: null },
+          { id: 2, type: 'urgent', value: 'New resume available', html: null },
+        ];
+
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications1} />);
+        const instance = wrapper.instance(); // Get the component instance
+
+        const shouldUpdateSpy = jest.spyOn(instance, 'shouldComponentUpdate');
+
+        // Update with a longer list
+        wrapper.setProps({ displayDrawer: true, listNotifications: listNotifications2 });
+
+        expect(shouldUpdateSpy).toHaveBeenCalledTimes(2); // It should rerender
+    });
+});
