34acfd75624c3207ae5ddba63609aa2584f2689b
Caleb Zwingli <84632961+ZwingliCaleb@users.noreply.github.com>
2023-09-14 23:48:48 +0300

    Update 1-map.js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,10 @@
 import { map } from 'immutable';
 
+/**
+ * Converts an object into an Immutable Map.
+ * @param {object} object - The input object to convert.
+ * @returns {Map} - An Immutable Map representation of the input object.
+ */
 export default function getImmutableObject(object){
 	return Map(object);
 }
