e556d74d20e6b7785d8156672f405ab012ce84ac
ZwingliCaleb <calebzwingli@gmail.com>
2023-09-15 18:49:12 +0300

    update tests

--- 0x05-react_state/task_2/dashboard/src/App/App.test.js
+++ 0x05-react_state/task_2/dashboard/src/App/App.test.js
@@ -9,73 +9,90 @@ import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 
 const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available', html: null },
-  { id: 2, type: 'urgent', value: 'New resume available', html: null },
-  { id: 3, type: 'urgent', value: null, html: { __html: 'Test HTML' } }
+  // ... (your notification data)
 ];
 
 describe('App tests', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<App />);
+  });
+
   it('renders without crashing', () => {
-    shallow(<App />)
+    expect(component).toBeTruthy();
   });
 
   it('renders the notification component', () => {
-    const component = shallow(<App />);
     expect(component.contains(<Notifications displayDrawer={true} listNotifications={listNotifications} />)).toBe(true);
   });
 
   it('renders the Header component', () => {
-    const component = shallow(<App />);
     expect(component.contains(<Header />)).toBe(true);
   });
 
   it('renders the login component', () => {
-    const component = shallow(<App />);
     expect(component.contains(<Login />)).toBe(true);
   });
 
   it('renders the Footer component', () => {
-    const component = shallow(<App />);
     expect(component.contains(<Footer />)).toBe(true);
   });
 
   it('verifies that CourseList is not rendered when isLoggedIn is false', () => {
-    const component = shallow(<App isLoggedIn={false}/>);
+    component.setState({ user: { email: '', password: '' } });
     expect(component.contains(<CourseList />)).toBe(false);
     expect(component.contains(<Login />)).toBe(true);
   });
 
   it('does not render Login when isLoggedIn is true', () => {
-    const component = shallow(<App isLoggedIn={true}/>);
+    component.setState({ user: { email: 'test@example.com', password: 'testpassword' } });
     expect(component.contains(<Login />)).toBe(false);
     expect(component.contains(<CourseList />)).toBe(true);
   });
 
-  it('verifies that when control + h are pressed the logOut function is called and shows the alert Logging you out', () => {
+  it('verifies that the default state for displayDrawer is false', () => {
+    expect(component.state('displayDrawer')).toBe(false);
+  });
+
+  it('verifies that after calling handleDisplayDrawer, the state should now be true', () => {
+    component.instance().handleDisplayDrawer();
+    expect(component.state('displayDrawer')).toBe(true);
+  });
+
+  it('verifies that after calling handleHideDrawer, the state is updated to be false', () => {
+    component.setState({ displayDrawer: true }); // Set initial state to true
+    component.instance().handleHideDrawer();
+    expect(component.state('displayDrawer')).toBe(false);
+  });
+
+  it('verifies that when control + h are pressed, logOut is called and shows the alert Logging you out', () => {
     const logOutMock = jest.fn();
     const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
 
     const wrapper = mount(<App logOut={logOutMock} />);
     const event = new KeyboardEvent('keydown', {
-      key: 'h', ctrlKey: true,
+      key: 'h',
+      ctrlKey: true,
     });
 
     document.dispatchEvent(event);
 
     expect(alertMock).toHaveBeenCalledWith('Logging you out');
     expect(alertMock).toHaveBeenCalledTimes(1);
+    expect(logOutMock).toHaveBeenCalled();
 
     alertMock.mockRestore();
     jest.clearAllMocks();
     wrapper.unmount();
   });
 
-  it('removing the event listener on mount', () => {
+  it('removing the event listener on unmount', () => {
     const logOutMock = jest.fn();
     const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
 
-    const component = mount(<App logOut={logOutMock} />);
-    component.unmount();
+    const wrapper = mount(<App logOut={logOutMock} />);
+    wrapper.unmount();
 
     const event = new KeyboardEvent('keydown', {
       key: 'h',
@@ -83,28 +100,32 @@ describe('App tests', () => {
     });
     document.dispatchEvent(event);
 
-    expect(alertMock).not.toHaveBeenCalled('');
-    expect(logOutMock).not.toHaveBeenCalled('');
+    expect(alertMock).not.toHaveBeenCalled();
+    expect(logOutMock).not.toHaveBeenCalled();
 
     alertMock.mockRestore();
     jest.clearAllMocks();
   });
 
-  it('verifies that the default state for displayDrawer is false', () => {
-    const component = shallow(<App />);
-    expect(component.state('displayDrawer')).toBe(false);
+  it('verifies that the logIn function updates the state correctly', () => {
+    const email = 'test@example.com';
+    const password = 'testpassword';
+
+    component.instance().logIn(email, password);
+
+    expect(component.state('user')).toEqual({
+      email,
+      password,
+    });
   });
 
-  it('verifies that after calling handleDisplayDrawer, the state should now be true', () => {
-    const component = shallow(<App />);
-    component.instance().handleDisplayDrawer();
-    expect(component.state('displayDrawer')).toBe(true);
-  });
+  it('verifies that the logOut function updates the state correctly', () => {
+    component.instance().logIn('test@example.com', 'testpassword');
+    component.instance().logOut();
 
-  it('verifies that after calling handleHideDrawer, the state is updated to be false', () => {
-    const component = shallow(<App />);
-    component.setState({ displayDrawer: true }); // Set initial state to true
-    component.instance().handleHideDrawer();
-    expect(component.state('displayDrawer')).toBe(false);
+    expect(component.state('user')).toEqual({
+      email: '',
+      password: '',
+    });
   });
 });
