0f2d5db5cac6b4cecce2f72dd585b2e342db3ef9
ZwingliCaleb <calebzwingli@gmail.com>
2023-09-15 09:40:13 +0000

    correct 3list.js to pass linter

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,6 +1,6 @@
 /*
-getListObject accepts an array as parameter and converts it into an
-immutable List using the Immutable.js library
+ * getListObject accepts an array as parameter and converts it into an
+ * immutable List using the Immutable.js library
 */
 const { List } = require('immutable');
 
@@ -11,4 +11,3 @@ export function getListObject(array) {
 export function addElementToList(list, element) {
   return list.push(element);
 }
-
