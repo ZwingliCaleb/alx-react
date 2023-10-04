79a4f959fd532dfc40b65b640d26e1be80b92440
ZwingliCaleb <calebzwingli@gmail.com>
2023-10-05 01:36:30 +0300

    Improve reducer. connect notificcations

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,14 +1,22 @@
-import { FETCH_NOTIFICATIONS_SUCCESS } from '../actions/notificationActionTypes';
+import { FETCH_NOTIFICATIONS_SUCCESS, SET_LOADING_STATE } from '../actions/notificationActionTypes';
 import { Map } from 'immutable';
 
 const initialState = Map({
   notifications: [],
+  loading: false,
 });
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_LOADING_STATE:
+      return state.set('loading', action.isLoading);
+
     case FETCH_NOTIFICATIONS_SUCCESS:
-      return state.set('notifications', action.payload);
+      return state.mergeDeep({
+        notifications: action.data,
+        loading: false,
+      });
+
     default:
       return state;
   }
