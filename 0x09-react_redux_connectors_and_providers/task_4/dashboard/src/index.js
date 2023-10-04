24a1758404246f9eb8fab37b8a53d2106ea631fb
ZwingliCaleb <calebzwingli@gmail.com>
2023-10-04 21:57:00 +0300

    Combine store: Root reducer

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import uiReducer from './reducers/uiReducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(uiReducer, composeEnhancers(applyMiddleware(thunk)));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
